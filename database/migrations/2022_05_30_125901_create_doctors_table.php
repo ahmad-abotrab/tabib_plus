abe84e7b7cc69c26fbb25f247400d3972e710529
AhmadAbotrab3099 <ahmad.abotrab3099@gmail.com>
2022-06-04 16:04:25 +0300

    editing for model

---
+++ database/migrations/2022_05_30_125901_create_doctors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('specialization');
+            $table->string('phoneNumber')->unique();
+            $table->string('password');
+            $table->string('birthDay');
+            $table->string('specialization');
+            $table->string('services');
+            $table->string('education');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
